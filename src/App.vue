3cbcbc0c22897631cc25e272e64b54551269ff0e
Derek Hassick <derek.hassick@gmail.com>
2019-08-29 16:25:30 -0600

    added raleway font to css.

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Raleway', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
