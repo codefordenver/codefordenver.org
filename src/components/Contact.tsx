f2eea315e5878108d0035b7ab02d8aec60d67078
David A. Viramontes <dviramontes@gmail.com>
2019-09-06 01:05:58 -0600

    feat: adds TextArea and rounds input fields

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { Button, Form, Grid, Header, Segment } from "semantic-ui-react";
+import React, { CSSProperties, useState } from "react";
+import { Button, Form, Grid, Header, Segment, TextArea } from "semantic-ui-react";
 import useAxios from "axios-hooks";
 
 export function Contact() {
     const to = "hello@codefordenver.org"
     const [name, setName] = useState<string>("")
     const [email, setEmail] = useState<string>("")
-    const [message, setMessage] = useState<string>("")
+    const [message, setMessage] = useState<any>("")
     const [
         { data: putData, loading: putLoading, error: putError },
         executePut
@@ -17,10 +17,11 @@ export function Contact() {
             to,
             from: email,
             subject: `Contact Form | ${name} `,
-            body: 'message'
+            body: message,
         },
     }, { manual: true })
 
+    const borderRadius__css: CSSProperties = { borderRadius: 18 }
 
     return (
         <Segment inverted vertical style={{ padding: "2em", backgroundColor: "#E14E54" }}>
@@ -30,11 +31,27 @@ export function Contact() {
                     <Form loading={putLoading}>
                         <Form.Field>
                             <label className="white">Name</label>
-                            <input placeholder='Name' onChange={(e) => setName(e.target.value)}/>
+                            <input
+                                style={borderRadius__css}
+                                placeholder='Name'
+                                onChange={(e) => setName(e.target.value)}/>
                         </Form.Field>
                         <Form.Field>
                             <label className="white">Email</label>
-                            <input placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
+                            <input
+                                style={borderRadius__css}
+                                placeholder='Email'
+                                onChange={(e) => setEmail(e.target.value)}/>
+                        </Form.Field>
+                        <Form.Field>
+                            <label className="white">Message Us</label>
+                            <TextArea
+                                style={borderRadius__css}
+                                maxlength={280}
+                                rows={5}
+                                cols={33}
+                                placeholder='in less than 280 characters please'
+                                onChange={(e, { value }) => setMessage(value)}/>
                         </Form.Field>
                         <Button inverted type='submit' onClick={() => executePut() }>Send us a message</Button>
                     </Form>
