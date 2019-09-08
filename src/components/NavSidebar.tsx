eb6cb6a6675ca02f7c13ef1d45729cb072e6c707
Josh Bernstein <joshua.bernstein@outlook.com>
2019-09-08 14:35:48 -0600

    Added boilerplate sidebar code

---
+++ src/components/NavSidebar.tsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import {
+  Button,
+  Header,
+  Icon,
+  Image,
+  Menu,
+  Segment,
+  Sidebar,
+  
+} from 'semantic-ui-react'
+
+export class SidebarExampleSidebar extends Component {
+  state = { visible: false }
+
+  handleHideClick = () => this.setState({ visible: false })
+  handleShowClick = () => this.setState({ visible: true })
+  handleSidebarHide = () => this.setState({ visible: false })
+
+  render() {
+    const { visible } = this.state
+
+    return (
+      <div>
+        <Button.Group>
+          <Button disabled={visible} onClick={this.handleShowClick}>
+            Show sidebar
+          </Button>
+          <Button disabled={!visible} onClick={this.handleHideClick}>
+            Hide sidebar
+          </Button>
+        </Button.Group>
+
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+              {this.props.children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    )
+  }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Banner } from "../components/Banner";
-import { Featured } from "../components/Featured"
+import { Featured } from "../components/Featured";
 import { Sponsors } from "../components/Sponsors";
 import { Volunteer } from "../components/Volunteer";
+import { SidebarExampleSidebar } from "../components/NavSidebar";
 
 export function Home() {
   return (
-    <>
-      <Banner/>
+    <SidebarExampleSidebar>
+      <Banner />
       <Volunteer />
       <Featured />
       <Sponsors />
-    </>
+    </SidebarExampleSidebar>
   );
 }
