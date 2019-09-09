891cb0e4591b8b1a2e103ec5926401ecad2994e1
David A. Viramontes <dviramontes@gmail.com>
2019-09-08 21:42:03 -0600

    FEAT: <Contact /> (#58)
    * begins: <Contact />
    
    * feat: adds TextArea and rounds input fields
    
    * fix: button size and styles
    
    * fix: show required
    
    * style: contanct form
    
    * feat: mark fields required, clear form onsubmit
    
    * wip: error handleling
    
    * feat: handle error
    
    * fix: reset msgs
    
    * chore: format
    
    * chore: add google maps api to prod env
    
    * fix: textarea css

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Sponsors } from "../components/Sponsors";
+import { Contact } from "../components/Contact";
 
 export function About() {
   return (
     <>
       <Sponsors />
+      <Contact />
     </>
   );
 }
