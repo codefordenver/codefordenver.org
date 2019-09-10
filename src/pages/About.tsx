0c0585ea2c3279770d2fc6c5eee353471c08bc98
Derek Hassick <derek.hassick@gmail.com>
2019-09-10 11:15:21 -0600

    created about view with components

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Sponsors } from "../components/Sponsors";
+import { AboutBanner } from "../components/AboutBanner";
+import { AboutOrg } from "../components/AboutOrg";
+import { AboutSponsor } from "../components/AboutSponsor";
+
 
 export function About() {
   return (
     <>
+      <AboutBanner />
+      <AboutOrg />
+      <AboutSponsor />
       <Sponsors />
     </>
   );
