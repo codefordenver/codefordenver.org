1c4c457881f8b2731f56bcc971810d0bfb4bb62d
Josh Bernstein <joshua.bernstein@outlook.com>
2019-09-09 21:01:00 -0600

    Updated banner to fit responsivly at 1024

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,37 +3,44 @@ import { Grid, Image, Header, Segment, Input, Button } from "semantic-ui-react";
 
 export function Banner() {
   return (
-    <Segment inverted vertical style={{ padding: "0em" }}>
-      <Grid columns='equal' stackable doubling>
-        <Grid.Row style={{ padding: "0em" }}>
-          <Grid.Column width={8}>
-            <Image src={process.env.PUBLIC_URL + "/images/denver-banner.jpg"} />
+    <>
+      <Segment inverted vertical style={{ padding: "0em" }}>
+        <Grid columns='equal' stackable doubling>
+          <Grid.Row style={{ padding: "0em" }}>
+            <Grid.Column width={8}>
+              <Image
+                fluid
+                src={process.env.PUBLIC_URL + "/images/denver-banner.jpg"}
+              />
+            </Grid.Column>
+            <Grid.Column verticalAlign='middle' className='columnTextRight'>
+              <p>
+                Code for Denver is building the next great generation of civic
+                hackers, innovators, and disruptors here in Colorado.
+              </p>
+              <p>
+                We are a Code for America Brigade community with regular meetups
+                and other events to help bring together creative problem solvers
+                with local community groups and government trying to address
+                some of our most pressing 21st century problems
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment>
+        <Header as='h4' content='Join our newsletter!' />
+        <Grid style={{ padding: "1em 0em" }}>
+          <Grid.Column floated='left' width={10}>
+            <Input fluid placeholder='Enter your email address' />
           </Grid.Column>
-          <Grid.Column verticalAlign='middle' className='columnTextRight'>
-            <p>
-              Code for Denver is building the next great generation of civic
-              hackers, innovators, and disruptors here in Colorado.
-            </p>
-            <p>
-              We are a Code for America Brigade community with regular meetups
-              and other events to help bring together creative problem solvers
-              with local community groups and government trying to address some
-              of our most pressing 21st century problems
-            </p>
-            <Header inverted as='h4' content='Join our newsletter!' />
-            <Grid style={{ padding: "1em 0em" }}>
-              <Grid.Column floated='left' width={10}>
-                <Input inverted fluid placeholder='Enter your email address' />
-              </Grid.Column>
-              <Grid.Column floated='right' width={6}>
-                <Button circular color='red'>
-                  Sign up!
-                </Button>
-              </Grid.Column>
-            </Grid>
+          <Grid.Column floated='right' width={6}>
+            <Button circular color='red'>
+              Sign up!
+            </Button>
           </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+        </Grid>
+      </Segment>
+    </>
   );
 }
