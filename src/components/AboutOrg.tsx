cd12a08c61345476d60e95372867683077461afa
Derek Hassick <derek.hassick@gmail.com>
2019-09-10 13:08:00 -0600

    moved donation and org comps all into one comp

--- src/components/AboutOrg.tsx
+++ src/components/AboutOrg.tsx
@@ -1,30 +1,47 @@
 import React from "react";
-import { Header, Grid, Segment, } from "semantic-ui-react";
+import { Button, Header, Grid, Segment, } from "semantic-ui-react";
 
 export function AboutOrg() {
 
   return (
     <Segment style={{ margin: "0", border: "0" }}>
-
       <Grid container stackable columns={2} className="bg-white">
-        <Grid.Column>
-          <Header as='h2'>Our Organization</Header>
-          <p>
-            We are a Code for America Brigade community with
-            regular meetups and other events to help bring together
-            creative problem solvers with local community groups
-            and government trying to address some of our most
-            pressing 21st century problems.
-          </p>
-        </Grid.Column>
-        <Grid.Column>
-          <Header as="h2">Our Vision</Header>
-          <p>
-            The Denver Community will be better served
-            by empowering local governments and nonprofits
-            through technology.
-          </p>
-        </Grid.Column>
+        <Grid.Row>
+          <Grid.Column>
+            <Header as='h2'>Our Organization</Header>
+            <p>
+              We are a Code for America Brigade community with
+              regular meetups and other events to help bring together
+              creative problem solvers with local community groups
+              and government trying to address some of our most
+              pressing 21st century problems.
+            </p>
+            <Button circular color='black'>
+              Join Us   
+            </Button>
+          </Grid.Column>
+          <Grid.Column>
+            <Header as="h2">Our Vision</Header>
+            <p>
+              The Denver Community will be better served
+              by empowering local governments and nonprofits
+              through technology.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <Header>Our Sponsors</Header>
+            <p>
+              We couldn’t do any of this work without our sponsors.
+              If you’d like to learn more about how to support Code
+              for Denver, reach out.
+            </p>
+            <Button circular color='black'>
+              Donate
+            </Button>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     </Segment>
   )
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import { Sponsors } from "../components/Sponsors";
 import { AboutBanner } from "../components/AboutBanner";
 import { AboutOrg } from "../components/AboutOrg";
-import { AboutSponsor } from "../components/AboutSponsor";
-
 
 export function About() {
   return (
     <>
       <AboutBanner />
       <AboutOrg />
-      <AboutSponsor />
+      
       <Sponsors />
     </>
   );
