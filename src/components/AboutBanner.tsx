6156f1e156f24d810295ec789385287082366510
Derek Hassick <derek.hassick@gmail.com>
2019-09-12 15:36:21 -0600

    resized font and background img

--- src/components/AboutBanner.tsx
+++ src/components/AboutBanner.tsx
@@ -5,32 +5,38 @@ export function AboutBanner() {
   
   return(
     <Grid 
-      verticalAlign='middle'
+      verticalAlign="middle"
       centered
       columns={1}
       style={{ margin: "0", border: "none" }}>
       <Grid.Row 
-        only='tablet computer'
+        only="tablet computer"
         style={{ 
-        height: "60vh",
+        height: "55vh",
         backgroundImage: `url("${process.env.PUBLIC_URL}/images/about-banner.jpeg")`,
-        backgroundPosition: 'center'
-        }}>
+        backgroundPosition: "center",
+        backgroundSize:"cover"
+      }}>
         <Grid.Column 
         style={{ 
           opacity: "0.9", 
           backgroundColor: "rgba(255, 255, 255, 0.7)",
-          width: "70vw"
+          width: "75vw"
           }}>
-            <Header as="h1" size="huge" style={{fontSize:"4rem"}} color="red">
+            <Header 
+            as="h1"
+            size="huge"
+            style={{fontSize:"2.7rem"}}
+            textAlign="center"
+            color="red">
               An intentionally inclusive community of problem solvers
             </Header>
         </Grid.Column>
       </Grid.Row>
-      <Grid.Row only='mobile' style={{backgroundColor:"black"}}>
+      <Grid.Row only="mobile" style={{backgroundColor:"black"}}>
         <Grid.Column
-          style={{ backgroundColor: "rgba(255, 255, 255, 0.7)" }}>
-          <Header as="h1" size="large" color="red">
+          style={{ backgroundColor: "rgba(255, 255, 255, 0.9)" }}>
+          <Header as="h1" size="large" color="red" textAlign="center">
             An intentionally inclusive community of problem solvers
           </Header>
         </Grid.Column>
