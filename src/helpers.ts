4f6ee9bc5d770de827a22d69514580f004df8798
David A. Viramontes <dviramontes@gmail.com>
2019-09-14 13:44:44 -0600

    feat: link out from featured project (#77)

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,6 +2,7 @@ import get from "lodash/get"
 
 // Normalize data from contentful API
 export function contentfulDataMapper(item: any, assets: Object[]) {
+    const link = get(item, "fields.link")
     const title = get(item, "fields.title")
     const shortDescription = get(item, "fields.shortDescription")
     const thumbId = get(item, "fields.thumbnail.sys.id")
@@ -9,6 +10,7 @@ export function contentfulDataMapper(item: any, assets: Object[]) {
     const thumbnail = get(thumbnailAsset, "fields.file.url")
 
     return {
+        link,
         title,
         thumbnail,
         shortDescription,
