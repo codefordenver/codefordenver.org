3d68875c551f0e37d152c117735b3f888c5dab4c
ZadenRB <zaden.ruggieroboune@gmail.com>
2019-09-19 21:02:08 -0600

    Fix buttons on volunteer page

--- src/pages/Volunteer.tsx
+++ src/pages/Volunteer.tsx
@@ -4,8 +4,9 @@ import { ExternalURL } from '../components/Router';
 
 export function Volunteer() {
   const button__css: CSSProperties = {
-    paddingRight: '2em',
-    paddingLeft: '2em'
+    paddingRight: '2rem',
+    paddingLeft: '2rem',
+    margin: '1rem .5rem 0rem'
   };
   const question__css: CSSProperties = { color: '#E14E54' };
 
@@ -91,29 +92,18 @@ export function Volunteer() {
               <a style={{ color: 'black' }} href={ExternalURL.FACEBOOK}>
                 <Icon size="large" name="facebook" />
               </a>
-              <Grid
-                columns={2}
-                style={{ margin: '3em' }}
-                stackable
-                textAlign="center"
-              >
-                <Grid.Row columns={2}>
-                  <Grid.Column>
-                    <a href={ExternalURL.MEETUP}>
-                      <Button circular color="black" style={button__css}>
-                        Meetup
-                      </Button>
-                    </a>
-                  </Grid.Column>
-                  <Grid.Column>
-                    <a href="contact">
-                      <Button circular color="black" style={button__css}>
-                        Contact
-                      </Button>
-                    </a>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
+              <div style={{ textAlign: 'center' }}>
+                <a href={ExternalURL.MEETUP}>
+                  <Button circular color="black" style={button__css}>
+                    Meetup
+                  </Button>
+                </a>
+                <a href="contact">
+                  <Button circular color="black" style={button__css}>
+                    Contact
+                  </Button>
+                </a>
+              </div>
             </Grid.Column>
             <Grid.Column>
               <div
