e7c1f23d06e84744135845c9158d1bd3e2876e06
ZadenRB <zaden.ruggieroboune@gmail.com>
2019-09-25 19:22:08 -0600

    Style mobile navigation when open

--- src/components/NavMenuItems.tsx
+++ src/components/NavMenuItems.tsx
@@ -11,36 +11,38 @@ export function NavMenuItems() {
         <FontAwesomeIcon icon={faBars} />
         <span>Menu</span>
       </span>
-      <div id="nav-buttons">
-        <NavHashLink
-          exact
-          to={PathURL.HOME}
-          activeClassName="active-nav-button"
-          className="nav-button"
-        >
-          Home
-        </NavHashLink>
-        <NavHashLink
-          to={PathURL.ABOUT}
-          activeClassName="active-nav-button"
-          className="nav-button"
-        >
-          About
-        </NavHashLink>
-        <NavHashLink
-          to={PathURL.VOLUNTEER}
-          activeClassName="active-nav-button"
-          className="nav-button"
-        >
-          Volunteer
-        </NavHashLink>
-        <a
-          id="donate-button"
-          className="pill-button"
-          href={ExternalURL.OPEN_COLLECTIVE}
-        >
-          Donate
-        </a>
+      <div id="nav-navigation" className="collapsed">
+        <div id="nav-buttons" className="visible">
+          <NavHashLink
+            exact
+            to={PathURL.HOME}
+            activeClassName="active-nav-button"
+            className="nav-button"
+          >
+            Home
+          </NavHashLink>
+          <NavHashLink
+            to={PathURL.ABOUT}
+            activeClassName="active-nav-button"
+            className="nav-button"
+          >
+            About
+          </NavHashLink>
+          <NavHashLink
+            to={PathURL.VOLUNTEER}
+            activeClassName="active-nav-button"
+            className="nav-button"
+          >
+            Volunteer
+          </NavHashLink>
+          <a
+            id="donate-button"
+            className="pill-button"
+            href={ExternalURL.OPEN_COLLECTIVE}
+          >
+            Donate
+          </a>
+        </div>
       </div>
     </>
   );
