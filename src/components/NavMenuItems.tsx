ce8b270d2032fb5a6151f8f3a4e26adf1c489172
ZadenRB <zaden.ruggieroboune@gmail.com>
2019-09-26 20:00:00 -0600

    Add functionality to nav menu trigger

--- src/components/NavMenuItems.tsx
+++ src/components/NavMenuItems.tsx
@@ -4,46 +4,94 @@ import { NavHashLink } from 'react-router-hash-link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-export function NavMenuItems() {
-  return (
-    <>
-      <span id="nav-trigger">
-        <FontAwesomeIcon icon={faBars} />
-        <span>Menu</span>
-      </span>
-      <div id="nav-navigation" className="collapsed">
-        <div id="nav-buttons" className="visible">
-          <NavHashLink
-            exact
-            to={PathURL.HOME}
-            activeClassName="active-nav-button"
-            className="nav-button"
-          >
-            Home
-          </NavHashLink>
-          <NavHashLink
-            to={PathURL.ABOUT}
-            activeClassName="active-nav-button"
-            className="nav-button"
-          >
-            About
-          </NavHashLink>
-          <NavHashLink
-            to={PathURL.VOLUNTEER}
-            activeClassName="active-nav-button"
-            className="nav-button"
-          >
-            Volunteer
-          </NavHashLink>
-          <a
-            id="donate-button"
-            className="pill-button"
-            href={ExternalURL.OPEN_COLLECTIVE}
-          >
-            Donate
-          </a>
+export class NavMenuItems extends React.Component {
+  render() {
+    return (
+      <>
+        <span id="nav-trigger" onClick={this.toggleNav}>
+          <FontAwesomeIcon icon={faBars} />
+          <span>Menu</span>
+        </span>
+        <div id="nav-navigation" className="collapsed">
+          <div id="nav-buttons" className="visible">
+            <NavHashLink
+              exact
+              to={PathURL.HOME}
+              activeClassName="active-nav-button"
+              className="nav-button"
+              onClick={this.hideNav}
+            >
+              Home
+            </NavHashLink>
+            <NavHashLink
+              to={PathURL.ABOUT}
+              activeClassName="active-nav-button"
+              className="nav-button"
+              onClick={this.hideNav}
+            >
+              About
+            </NavHashLink>
+            <NavHashLink
+              to={PathURL.VOLUNTEER}
+              activeClassName="active-nav-button"
+              className="nav-button"
+              onClick={this.hideNav}
+            >
+              Volunteer
+            </NavHashLink>
+            <a
+              id="donate-button"
+              className="pill-button"
+              href={ExternalURL.OPEN_COLLECTIVE}
+            >
+              Donate
+            </a>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleDocumentClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleDocumentClick);
+  }
+
+  handleDocumentClick = (e: Event) => {
+    let nav = document.getElementById('nav-navigation');
+    let navTrigger = document.getElementById('nav-trigger');
+    if (
+      nav != null &&
+      e.target instanceof Element &&
+      !nav.contains(e.target as Element) &&
+      navTrigger != null &&
+      !navTrigger.contains(e.target) &&
+      navTrigger !== e.target
+    ) {
+      this.hideNav();
+    }
+  };
+
+  toggleNav = () => {
+    let nav = document.getElementById('nav-navigation');
+    if (nav != null) {
+      nav.classList.toggle('visible');
+      if (nav.classList.contains('visible')) {
+        document.addEventListener('click', this.handleDocumentClick);
+      } else {
+        document.removeEventListener('click', this.handleDocumentClick);
+      }
+    }
+  };
+
+  hideNav = () => {
+    let nav = document.getElementById('nav-navigation');
+    if (nav != null) {
+      nav.classList.remove('visible');
+      document.removeEventListener('click', this.handleDocumentClick);
+    }
+  };
 }
