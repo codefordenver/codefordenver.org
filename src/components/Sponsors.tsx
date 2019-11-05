2c2948cbfe4c87236841817333de68572bdc49cf
ZadenRB <zaden.ruggieroboune@gmail.com>
2019-11-04 20:32:47 -0700

    WIP: Style Home page. TODO: Sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -2,60 +2,40 @@ import React, { CSSProperties } from 'react';
 import { Grid, Image, Header, Segment } from 'semantic-ui-react';
 import { ExternalURL } from './Router';
 
-type Props = {
-  show?: boolean;
-};
-
-export function Sponsors(props: Props) {
+export function Sponsors() {
   const image__css: CSSProperties = {
     margin: '0 auto',
     maxWidth: '250px',
     padding: 0
   };
-  const showHeader = onAbout();
 
-  function onAbout() {
-    if (props.show === null) {
-      return <Header as="h3" content="Our sponsors" />;
-    }
-  }
   return (
-    <Segment style={{ margin: '0' }}>
-      <Grid style={{ padding: '5em 0' }} columns="equal" stackable>
-        {showHeader}
-        <Grid.Row textAlign="center" verticalAlign="middle" columns={3}>
-          <Grid.Column style={{ padding: 0 }}>
-            <Image
-              as="a"
-              href={ExternalURL.GARYCOMMUNITY}
-              style={image__css}
-              src={
-                process.env.PUBLIC_URL +
-                '/images/sponsors/sponsor-gary_community_investments-300x236.png'
-              }
-            />
-          </Grid.Column>
-          <Grid.Column style={{ padding: 0 }}>
-            <Image
-              as="a"
-              href={ExternalURL.GALVANIZE}
-              style={image__css}
-              src={process.env.PUBLIC_URL + '/images/sponsors/galvanize.png'}
-            />
-          </Grid.Column>
-          <Grid.Column style={{ padding: 0 }}>
-            <Image
-              as="a"
-              href={ExternalURL.JETBRAINS}
-              style={image__css}
-              src={
-                process.env.PUBLIC_URL +
-                '/images/sponsors/jetbrains-variant-4-700x391.png'
-              }
-            />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+    <>
+      <h3>Our sponsors</h3>
+      <a href={ExternalURL.GARYCOMMUNITY}>
+        <img
+          style={image__css}
+          src={
+            process.env.PUBLIC_URL +
+            '/images/sponsors/sponsor-gary_community_investments-300x236.png'
+          }
+        />
+      </a>
+      <a href={ExternalURL.GALVANIZE}>
+        <img
+          style={image__css}
+          src={process.env.PUBLIC_URL + '/images/sponsors/galvanize.png'}
+        />
+      </a>
+      <a href={ExternalURL.JETBRAINS}>
+        <img
+          style={image__css}
+          src={
+            process.env.PUBLIC_URL +
+            '/images/sponsors/jetbrains-variant-4-700x391.png'
+          }
+        />
+      </a>
+    </>
   );
 }
