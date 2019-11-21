c9abb07ea979c7a0ae229b853063903bea3cdd77
Derek Hassick <derek.hassick@gmail.com>
2019-11-21 14:44:20 -0700

    created proj card and style, added dynamic components

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -5,9 +5,9 @@ import get from 'lodash.get';
 import { contentfulDataMapper } from '../helpers';
 
 type Props = {
-  title?: string;
-  shortDescription?: string;
-  thumbnail?: string;
+  title: string;
+  shortDescription: string;
+  thumbnail: string;
   link?: string;
   screenShots?: string;
   tech?: string;
@@ -16,31 +16,31 @@ type Props = {
 
 const ScreenShots = (props: Props) => (
   <Grid.Column>
-    <Image src={props.screenShots} size="small" width={200} />
+    <Image src={props.screenShots} size="small" width={100} />
   </Grid.Column>
 );
 
 export function ProjectCard(props: Props) {
   return (
   <Segment>
-     <div>
-       <Grid columns={2} centered>
+     <div style={{padding:"5vh 10vw"}}>
+       <Grid columns={2} verticalAlign="middle" stackable>
          <Grid.Column >
-            <Header as='h2' style={{ color: ' #E14E54'}}>Project</Header>
-            <Header as='h2' style={{ fontWeight: '800'}}>{props.title}</Header>
-            <Header as='h2' style={{ color: ' #E14E54' }}>Sponsor</Header>
-          <Header as='h2'>{props.sponsor}</Header>
+            <Header as='h2' style={{ color: '#E14E54'}} >Project</Header>
+            <Header as='h2' style={{ fontWeight: '800'}} >{props.title}</Header>
+            <Header as='h2' style={{ color: '#E14E54' }} >Sponsor</Header>
+            <Header as='h2'>{props.sponsor}</Header>
         </Grid.Column>
         <Grid.Column>
-            <Image src={props.thumbnail} size='large' centered />
+            <Image src={props.thumbnail} alt={props.title} Logo size='medium' centered />
           </Grid.Column>
         </Grid>
-      </div>  
-      <p>{props.shortDescription}</p> 
-      <Header as='h3'>Technology used</Header>
-      <p>{props.tech} TECH USED DISPLAYED HERE</p>
-        <Grid style={{ backgroundColor: '#E14E54'}}>
-        <Header as='h3' style={{ color: '#FFFFFF'}}>{props.title} Screenshots</Header>     
+        <p style={{marginTop:'7vh' }}>{props.shortDescription}</p> 
+        <Header as='h6'>Technology used</Header>
+        <p>{props.tech} TECH USED DISPLAYED HERE</p>
+      </div >  
+      <Grid style={{ backgroundColor: '#E14E54', padding:"2vh 10vw" }}>
+        <Header as='h3' className="white">{props.title} Screenshots</Header>     
           <Grid divided inverted stackable>
             <Grid.Row columns="equal">
               {/* {props.screenShots.map((c: any, i: number) => (
@@ -52,6 +52,3 @@ export function ProjectCard(props: Props) {
     </Segment>
     );
   }
-// <Menu.Item as={Link} to={`${PathURL.PROJECTS}/project_one`}>
-//   Example Project 1
-//       </Menu.Item>
