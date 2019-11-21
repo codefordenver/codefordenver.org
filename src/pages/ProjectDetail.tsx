3007c8aa8faa4115df07151d75db54583e2a93ba
Derek Hassick <derek.hassick@gmail.com>
2019-11-21 15:42:08 -0700

    removed Project Detail pag, added header & project component map

--- src/pages/ProjectDetail.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-
-type TParams = { name: string };
-
-export function Project({ match }: RouteComponentProps<TParams>) {
-  return <h2>This is a page for product with ID: {match.params.name} </h2>;
-}
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Menu } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
-import { PathURL } from '../components/Router';
+import React, { useContext } from 'react';
+import ContentfulContext from '../context/contentful';
+import { contentfulDataMapper } from '../helpers';
+import get from 'lodash.get';
+import { ProjectCard } from '../components/ProjectCard';
+import { Header } from 'semantic-ui-react';
 
 export function Projects() {
-  return (
-    <>
-      <Menu.Item as={Link} to={`${PathURL.PROJECTS}/project_one`}>
-        Example Project 1
-      </Menu.Item>
+  const content = useContext(ContentfulContext);
+  if (content) {
+    const items = get(content, 'items', []).slice(0, 3);
+    const assets = get(content, 'includes.Asset', []);
 
-      <Menu.Item as={Link} to={`${PathURL.PROJECTS}/project_two`}>
-        Example Project 2
-      </Menu.Item>
-    </>
-  );
+    return (
+      <div className="bg-white">
+        <Header
+          style={{ padding: '1rem 2rem', fontSize: "2rem" }}
+          as="h1"
+          size="huge">
+          Featured Projects
+        </Header>
+        <article style={{ border: 'none' }}>
+          {items.map((c: any, i: number) => (
+            <ProjectCard key={i} {...contentfulDataMapper(c, assets)} />
+          ))}
+        </article>
+
+      </div>
+    );
+  }
+  return null;
 }
