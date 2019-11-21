0f76921c68d09d2a9062963de8dc699de11c55c6
Derek Hassick <derek.hassick@gmail.com>
2019-11-21 14:42:24 -0700

    added header and mapping over project comp

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,27 +1,33 @@
 
-// featured project page
 import React, { useContext } from 'react';
 import { ProjectCard } from '../components/ProjectCard';
 import ContentfulContext from '../context/contentful';
 import get from 'lodash.get';
 import { contentfulDataMapper } from '../helpers';
+import { Header } from 'semantic-ui-react';
 
 export function Projects() {
- 
   const content = useContext(ContentfulContext);
   if (content) {
     const items = get(content, 'items', []).slice(0, 3);
     const assets = get(content, 'includes.Asset', []);
 
     return (
-      <article>
-              {items.map((c: any, i: number) => (
-                <ProjectCard key={i} {...contentfulDataMapper(c, assets)} />
-              ))}
-      </article>
+      <div className="bg-white">
+        <Header
+          style={{ margin: "3vh 10vw 0.5vh", fontSize: "2rem" }}
+          as="h1"
+          size="huge">
+          Featured Projects
+        </Header>
+        <article style={{border:'none'}}>
+          {items.map((c: any, i: number) => (
+            <ProjectCard key={i} {...contentfulDataMapper(c, assets)} />
+          ))}
+        </article>
+
+      </div>
     );
   }
   return null;
-
 }
-
