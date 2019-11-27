c4d46301a59cdb5d517b0e28258ea881e3107a73
Derek Hassick <derek.hassick@gmail.com>
2019-11-27 12:31:44 -0700

    displays screenshots and tech

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,22 +1,19 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Header, Segment, Image, Grid} from 'semantic-ui-react';
-import ContentfulContext from '../context/contentful';
-import get from 'lodash.get';
-import { contentfulDataMapper } from '../helpers';
 
 type Props = {
   title: string;
   shortDescription: string;
   thumbnail: string;
-  link?: string;
-  screenShots?: string;
-  tech?: string;
-  sponsor?: string;
+  partner: string;
+  link: string;
+  tech: string;
+  screenShots: string[];
 };
 
-const ScreenShots = (props: Props) => (
-  <Grid.Column>
-    <Image src={props.screenShots} size="small" width={100} />
+const ScreenShots = (shot:string, i:number) => (
+  <Grid.Column key={i}>
+    <Image src={shot} size="small" width={100} />
   </Grid.Column>
 );
 
@@ -26,29 +23,27 @@ export function ProjectCard(props: Props) {
      <div style={{padding:"5vh 10vw"}}>
        <Grid columns={2} verticalAlign="middle" stackable>
          <Grid.Column >
-            <Header as='h2' style={{ color: '#E14E54'}} >Project</Header>
-            <Header as='h2' style={{ fontWeight: '800'}} >{props.title}</Header>
-            <Header as='h2' style={{ color: '#E14E54' }} >Sponsor</Header>
-            <Header as='h2'>{props.sponsor}</Header>
+            <Header as='h3' style={{ color: '#E14E54'}} >Project</Header>
+            <Header as='h4' style={{ fontWeight: '800'}} >{props.title}</Header>
+            <Header as='h3' style={{ color: '#E14E54' }} >Sponsor</Header>
+            <p >{props.partner}</p>
         </Grid.Column>
         <Grid.Column>
             <Image src={props.thumbnail} alt={props.title} Logo size='medium' centered />
           </Grid.Column>
         </Grid>
         <p style={{marginTop:'7vh' }}>{props.shortDescription}</p> 
-        <Header as='h6'>Technology used</Header>
-        <p>{props.tech} TECH USED DISPLAYED HERE</p>
+        <Header as='h5'>Technology and Tools Used:</Header>
+        <p>{props.tech}</p>
       </div >  
-      <Grid style={{ backgroundColor: '#E14E54', padding:"2vh 10vw" }}>
+      <div style={{ backgroundColor: '#E14E54', padding:"2vh 10vw" }}>
         <Header as='h3' className="white">{props.title} Screenshots</Header>     
           <Grid divided inverted stackable>
             <Grid.Row columns="equal">
-              {/* {props.screenShots.map((c: any, i: number) => (
-                <ScreenShots key={i} {...contentfulDataMapper(c, assets)} />
-              ))} */}
+              {props.screenShots.map((shot: any, i: number) => ScreenShots(shot, i) )}
             </Grid.Row>
           </Grid>
-      </Grid>
+      </div>
     </Segment>
     );
   }
