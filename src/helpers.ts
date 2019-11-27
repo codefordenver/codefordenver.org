85f45cc255ca83df5d992bd00056eb80260cb407
Derek Hassick <derek.hassick@gmail.com>
2019-11-27 10:39:36 -0700

    added tech and links

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,15 +5,24 @@ export function contentfulDataMapper(item: any, assets: Object[]) {
   const link = get(item, 'fields.link');
   const title = get(item, 'fields.title');
   const shortDescription = get(item, 'fields.shortDescription');
+  const partner = get(item, 'fields.partner');
+  const tech = get(item, 'fields.tech');
   const thumbId = get(item, 'fields.thumbnail.sys.id');
   const thumbnailAsset = assets.find((a: any) => get(a, 'sys.id') === thumbId);
   const thumbnail = get(thumbnailAsset, 'fields.file.url');
+  // const screenShotsId = get(item, 'fields.screenShots.sys.id');
+  // const screenShotsAssets = assets.find((a: any) => get(a, 'sys.id') === screenShotsId);
+  // const screenShots = get(screenShotsAssets, 'fields.file.url');
+
 
   return {
     link,
     title,
     thumbnail,
-    shortDescription
+    shortDescription,
+    partner,
+    // screenShotsId,
+    tech
   };
 }
 
