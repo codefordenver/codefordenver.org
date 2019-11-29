717d5b540f9e420218a9bec4c773af46ceb600d3
Derek Hassick <derek.hassick@gmail.com>
2019-11-29 15:21:14 -0700

    added hash links to each featured project

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -2,18 +2,30 @@ import React, { useContext } from 'react';
 import ContentfulContext from '../context/contentful';
 import get from 'lodash.get';
 import { contentfulDataMapper } from '../helpers';
+import { HashLink } from 'react-router-hash-link';
 
 interface IProject {
   title: string;
   shortDescription: string;
   thumbnail: string;
+  link: string;
+  projId: string;
 }
 
 const Project = (props: IProject) => (
   <span className="project">
-    <img alt="Project Logo" src={props.thumbnail} />
-    <h4>{props.title}</h4>
-    <p>{props.shortDescription}</p>
+    <HashLink
+      className="hash_link_white"
+      aria-label={`${props.title} project link`}
+      to={`/projects/#${props.projId}`}
+      scroll={(el: Element) =>
+        el.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      }
+    >
+      <img alt={`${props.title} Project Logo`} src={props.thumbnail} />
+      <h4>{props.title}</h4>
+      <p>{props.shortDescription}</p>
+    </HashLink>
   </span>
 );
 
@@ -30,7 +42,11 @@ export function Featured() {
           <h2>Featured Projects</h2>
           <div className="row">
             {items.map((c: any, i: number) => (
-              <Project key={i} {...contentfulDataMapper(c, assets)} />
+              <Project
+                key={i}
+                {...contentfulDataMapper(c, assets)}
+                projId={`${i}`}
+              />
             ))}
           </div>
         </div>
