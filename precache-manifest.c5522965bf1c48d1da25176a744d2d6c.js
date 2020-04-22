8fc236372a8392e71f57e59afeac5e2d8967b009
dviramontes <dviramontes@gmail.com>
2020-04-22 19:52:01 +0000

    Updates

---
+++ precache-manifest.c5522965bf1c48d1da25176a744d2d6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcb452436dcae4d0602919a6e3ea20a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cabac88178a62eb9218",
+    "url": "/static/css/main.a73f3db1.chunk.css"
+  },
+  {
+    "revision": "cb0f4e0d02bff4cb836d",
+    "url": "/static/js/2.0420f1e8.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.0420f1e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cabac88178a62eb9218",
+    "url": "/static/js/main.be76016f.chunk.js"
+  },
+  {
+    "revision": "cc41507827e4f0603b41",
+    "url": "/static/js/runtime-main.2b1c4d71.js"
+  }
+]);
